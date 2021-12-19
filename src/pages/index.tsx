78290358f2ff79ee960038e2d36ecf9708b45a67
Fairy-Phy <phy.public@gmail.com>
2021-12-19 23:50:26 +0900

    create project

---
+++ src/pages/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import clsx from 'clsx';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import styles from './index.module.css';
+import HomepageFeatures from '../components/HomepageFeatures';
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title">ここは何もないよ</h1>
+        <p className="hero__subtitle">特に意味はないよ</p>
+      </div>
+    </header>
+  );
+}
+
+export default function Home(): JSX.Element {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={`ほーむ`}
+      description="">
+      <HomepageHeader />
+      <main>
+      </main>
+    </Layout>
+  );
+}
